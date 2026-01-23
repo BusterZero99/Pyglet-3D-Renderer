a6ab0dda4f270de9934af69fb253f155609bb68b
BusterZero99 <desilvalaurence+github@gmail.com>
2026-01-23 12:51:55 +0000

    Optimised.

--- src/math_utils.py
+++ src/math_utils.py
@@ -1,17 +1,16 @@
 import numpy as np
 
 def perspective(fov, aspect, near, far):
-    """Create a perspective projection matrix."""
     f = 1.0 / np.tan(fov / 2)
+    nf = 1 / (near - far)
     return np.array([
         [f/aspect, 0, 0, 0],
         [0, f, 0, 0],
-        [0, 0, (far+near)/(near-far), (2*far*near)/(near-far)],
+        [0, 0, (far + near) * nf, 2 * far * near * nf],
         [0, 0, -1, 0]
     ], dtype=np.float32)
 
 def rotation_x(angle):
-    """Rotation matrix around the X axis."""
     c, s = np.cos(angle), np.sin(angle)
     return np.array([
         [1, 0, 0, 0],
@@ -21,7 +20,6 @@ def rotation_x(angle):
     ], dtype=np.float32)
 
 def rotation_y(angle):
-    """Rotation matrix around the Y axis."""
     c, s = np.cos(angle), np.sin(angle)
     return np.array([
         [c, 0, s, 0],
@@ -31,7 +29,6 @@ def rotation_y(angle):
     ], dtype=np.float32)
 
 def rotation_z(angle):
-    """Rotation matrix around the Z axis."""
     c, s = np.cos(angle), np.sin(angle)
     return np.array([
         [c, -s, 0, 0],
@@ -41,7 +38,6 @@ def rotation_z(angle):
     ], dtype=np.float32)
 
 def translation_matrix(offset):
-    """Translation matrix for a 3D offset (x,y,z)."""
     m = np.eye(4, dtype=np.float32)
     m[:3, 3] = offset
     return m
